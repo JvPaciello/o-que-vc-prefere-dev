0ce64d364a5cf162c1b3ca3562f0cb9f2a8a13e8
Gabriel Nunes <gnunesmail@gmail.com>
2026-01-17 16:08:57 -0300

    Revert "fix"
    This reverts commit 8e39301e20314d030adfb02d25838195cf7f82d8.

--- api/index.js
+++ api/index.js
@@ -6,10 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
-});
-
 const db = new Database('dev.db');
 
 db.exec(`
